e2339947b2ac7a31f2e9b0c81507a6cf91148743
Hilary Natalie Tabra <Misspurple@hilarys-air.home>
2022-05-29 19:02:50 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+
+package task1;
+
+class Main {
+    public static void main(String[] args) {
+        try {
+            NullPointer.Error();
+        } catch (NullPointerException e) {
+            e.getMessage();
+        }
+    }
+}
+
+class NullPointer {
+    public static void Error() {
+        int number = (Integer) null;
+    }
+}
